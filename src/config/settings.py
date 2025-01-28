026e3e36e0146dc5a584dc370401656e13900458
sonya <sonyakarm@icloud.com>
2025-01-28 17:30:31 +0300

    updated github actions settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -64,7 +64,6 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "HOST": os.getenv("DB_HOST"),
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
