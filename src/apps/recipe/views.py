622ec9cb1812fd090c4f9ec5b049995465af9aa3
sonya <sonyakarm@icloud.com>
2025-01-31 11:04:19 +0300

    Re-factored tests and ingredients

--- src/apps/recipe/views.py
+++ src/apps/recipe/views.py
@@ -6,38 +6,29 @@ from core.models import Tag, Ingredient
 from recipe import serializers
 
 
-class TagViewSet(viewsets.ModelViewSet):
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
+                                mixins.ListModelMixin,
+                                mixins.CreateModelMixin):
+        """Базовый класс для управления атрибутами рецепта."""
+        authentication_classes = (TokenAuthentication,)
+        permission_classes = (IsAuthenticated,)
+
+        def get_queryset(self):
+            """Возвращает объекты для текущего пользователя."""
+            return self.queryset.filter(user=self.request.user).order_by('name')
+
+        def perform_create(self, serializer):
+            """Создание нового объекта."""
+            serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
     """Управление рецептами."""
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
 
-    def get_queryset(self):
-        """Возвращает теги для текущего пользователя."""
-        queryset = self.queryset
-        assigned_only = bool(self.request.query_params.get('assigned_only'))  # преобразование строки в булево значение
-        if assigned_only:
-            queryset = queryset.filter(recipe__isnull=False)
 
-        return queryset.filter(user=self.request.user)
-
-    def perform_create(self, serializer):
-        """Создание нового тега."""
-        serializer.save(user=self.request.user)
-
-
-class IngredientViewSet(viewsets.ModelViewSet):
+class IngredientViewSet(BaseRecipeAttrViewSet):
     """Управление ингредиентами."""
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
-
-    def get_queryset(self):
-        """Возвращает ингредиенты для текущего пользователя."""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """Создание нового ингредиента."""
-        serializer.save(user=self.request.user)
